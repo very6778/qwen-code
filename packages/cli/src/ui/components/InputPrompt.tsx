9a6c3956e248945a703665092bd5c628ac15a701
sait <sait@example.com>
2025-10-30 02:09:25 +0300

    Further lighten input prompt background

--- packages/cli/src/ui/components/InputPrompt.tsx
+++ packages/cli/src/ui/components/InputPrompt.tsx
@@ -69,7 +69,7 @@ export const InputPrompt: React.FC<InputPromptProps> = ({
   vimHandleInput,
 }) => {
   const normalizedFrameWidth = Math.max(0, Math.floor(frameWidth));
-  const promptBackgroundColor = '#2c2c2c';
+  const promptBackgroundColor = '#353535';
   const promptTextColor = '#d4d4d4';
   const placeholderColor = '#999797';
   const [justNavigatedHistory, setJustNavigatedHistory] = useState(false);
